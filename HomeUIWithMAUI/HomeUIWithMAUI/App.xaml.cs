96f3f4bbc1b6a5ed844a00ff67b42edf95113470
Andrew Gordon <65138166+andrewgg1@users.noreply.github.com>
2024-10-12 15:54:43 -0400

    .NET MAUI App created
    - Base MAUI App created with sample app

---
+++ HomeUIWithMAUI/HomeUIWithMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeUIWithMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
