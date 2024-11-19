6cf1ac80b72c556d8ff54e89443fed1e79085fa9
Shy <85186628+Shy2003@users.noreply.github.com>
2024-11-19 10:14:42 -0500

    Program.cs changing default exposed port for container.

--- HomeUIWithMAUI/HomeAutomationAPI/Program.cs
+++ HomeUIWithMAUI/HomeAutomationAPI/Program.cs
@@ -1,11 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure Kestrel to listen on port 80 for Docker
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80); // The container's default exposed port
+});
 
 var app = builder.Build();
 
